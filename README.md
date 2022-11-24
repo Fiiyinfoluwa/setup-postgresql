814498e18de74fbf92b60c2ca104d712ad3bd80d
Fiyinfoluwa Fatuase <fatuasefiyinfoluwa@gmail.com>
2022-11-24 15:20:16 +0100

    Pgsql-script

---
+++ README.md
@@ -0,0 +1,11 @@
+# Installation and Set up of PostgreSQL using BASH SCRIPT
+
+## Pgsql-non-interactive.sh
+
+Pgsql-non-interactive.sh installs PostgreSQL and creates pgsql user "Seyi" and database "ExampleDB".
+
+Pgsql-non-interactive.sh can be deployed via ansible as it doesn't require user interaction
+
+## Pgsql-interactive.sh
+
+Pgsql-interactive.sh installs PostgreSQL and requests input from the user to create to enter name for desired pgsql user to be created, database name and user the database should be connected to.
---
+++ pgsql-interactive.sh
@@ -0,0 +1,89 @@
+#Installation of PostgreSQL and SetUp
+echo "Installing software prerequsites..."
+sudo apt install wget ca-certificates -y
+echo "DONE"
+echo "----"
+echo "  "
+sleep 5
+
+echo "Adding Key"
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+echo "DONE"
+echo "----"
+echo "  "
+sleep 5
+
+echo "Add PgSQL repo to sources list"
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
+echo "DONE"
+echo "----"
+echo "  "
+sleep 5
+
+echo "Installing PostgreSQL"
+sudo apt update -y && sudo apt upgrade -y && sudo apt install postgresql postgresql-contrib -y
+echo "DONE"
+echo "----"
+echo "  "
+sleep 5
+
+#Setting Up PostgreSQL Server
+echo "Allowing Remote Access...."
+sudo sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/" /etc/postgresql/15/main/postgresql.conf
+echo "DONE"
+echo "----"
+echo "  "
+sleep 5
+
+echo "Allowing TCP/IP connections to all databases for all users with any IPV4 address using an MD5 password"
+sudo sh -c 'echo "host    replication     all             0.0.0.0/0               md5" >> /etc/postgresql/15/main/pg_hba.conf'
+echo "DONE"
+echo "----"
+echo "  "
+sleep 5
+
+echo "Allowing connections to all databases for all local users using an MD5 password"
+sudo sed -i "s/local   all             all                                     peer/local   all             all                                     md5/" /etc/postgresql/15/main/pg_hba.conf
+echo "DONE"
+echo "----"
+echo "  "
+sleep 5
+
+echo "Allowing connections to all databases for all default user postgres without password"
+sudo sed -i "s/local   all             postgres                                peer/local   all             postgres                                trust/" /etc/postgresql/15/main/pg_hba.conf
+echo "DONE"
+echo "----"
+echo "  "
+sleep 5
+
+#Restart PostgreSQL
+echo "Restarting PostgreSQL Service to reload configurations...."
+sudo systemctl restart postgresql
+echo "DONE"
+echo "----"
+echo "  "
+sleep 5
+
+
+#Setting Up New User for PostgreSQL
+
+echo "Create New User in PostgreSQL"
+echo "Enter Username: "
+read User
+sudo -u postgres createuser --interactive $User -P
+echo "DONE"
+echo "----"
+echo "  "
+
+
+#Setting Up New Database
+echo "Enter Database Name: "
+read database
+
+echo "Enter existing user to connect database with: "
+read user 
+
+sudo createdb -U $user $databasename 
+echo "DONE"
+echo "----"
+echo "  "
